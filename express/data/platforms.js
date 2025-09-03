1fe67baa57072160d72b40f9ba0f5c6104ff0eb8
LucaCiani <lucaciani88@gmail.com>
2025-09-03 13:01:48 +0200

    add platforms

---
+++ express/data/platforms.js
@@ -0,0 +1,8 @@
+const platforms = [
+    { id: 1, name: "PC" },
+    { id: 2, name: "PlayStation" },
+    { id: 3, name: "Xbox" },
+    { id: 4, name: "Nintendo Switch" },
+];
+
+export default platforms;
