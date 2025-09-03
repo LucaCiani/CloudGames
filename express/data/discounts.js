293f54c29c4c89422d1535998199d51ba855c105
angelo-lepore <angelo.lepore94@gmail.com>
2025-09-03 12:59:46 +0200

    add discounts.js

---
+++ express/data/discounts.js
@@ -0,0 +1,74 @@
+const discounts = [
+  {
+    id: 1,
+    code: "WELCOME5",
+    discount_percentage: 5,
+    valid_from: "2025-01-01",
+    expires_at: "2025-12-31",
+  },
+  {
+    id: 2,
+    code: "GAMER10",
+    discount_percentage: 10,
+    valid_from: "2025-08-01",
+    expires_at: "2025-09-30",
+  },
+  {
+    id: 3,
+    code: "BLACKFRIDAY20",
+    discount_percentage: 20,
+    valid_from: "2024-11-20",
+    expires_at: "2024-11-30",
+  },
+  {
+    id: 4,
+    code: "SUMMER15",
+    discount_percentage: 15,
+    valid_from: "2025-06-01",
+    expires_at: "2025-08-31",
+  },
+  {
+    id: 5,
+    code: "SPRING7",
+    discount_percentage: 7,
+    valid_from: "2025-03-01",
+    expires_at: "2025-04-30",
+  },
+  {
+    id: 6,
+    code: "XMAS25",
+    discount_percentage: 25,
+    valid_from: "2025-12-01",
+    expires_at: "2025-12-31",
+  },
+  {
+    id: 7,
+    code: "NEWYEAR30",
+    discount_percentage: 30,
+    valid_from: "2025-12-30",
+    expires_at: "2026-01-05",
+  },
+  {
+    id: 8,
+    code: "RETRO8",
+    discount_percentage: 8,
+    valid_from: "2025-07-01",
+    expires_at: "2025-09-15",
+  },
+  {
+    id: 9,
+    code: "FPS12",
+    discount_percentage: 12,
+    valid_from: "2024-09-01",
+    expires_at: "2024-10-01",
+  },
+  {
+    id: 10,
+    code: "RPG18",
+    discount_percentage: 18,
+    valid_from: "2025-09-01",
+    expires_at: "2025-10-15",
+  },
+];
+
+export default discounts;
