65ce0761e311fd3bc70257b1982937b36b06400e
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-09-03 17:38:26 +0200

    add blurtext

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,7 +1,22 @@
+import BlurText from "./BlurText";
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
+
 export default function HeaderComponent() {
   return (
     <>
       <h1>Sono un header</h1>
+      <img src="/jumbotron.png" alt="jumbotron" />
+      <BlurText
+  text="Isn't this so cool?!"
+  delay={150}
+  animateBy="words"
+  direction="top"
+  onAnimationComplete={handleAnimationComplete}
+  className="text-2xl mb-8"
+/>
     </>
   );
 }
