6157e17de8d6baa742a89a5491b3f6db4b68a896
francescoboschelle <francibossch@gmail.com>
2025-09-05 10:36:04 +0200

    Refactor videogames controller and routes: enhance index and show functions, remove unused CRUD operations

--- express/controllers/videogamesController.js
+++ express/controllers/videogamesController.js
@@ -34,8 +34,6 @@ function index(req, res) {
     if (err) return res.status(500).json({ error: "Database query failed" });
     // se non ci sono errori, restituiamo i risultati della query in formato JSON
 
-    console.log(results);
-
     const formattedResult = results.map((result) => {
       return {
         id: result.id,
@@ -73,123 +71,80 @@ function index(req, res) {
 /* show (read) */
 function show(req, res) {
   // estrae l'ID dalla URL e lo converte da stringa a numero
-  const id = parseInt(req.params.id);
+  const { id } = req.params;
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid ID" });
   // definiamo la query SQL per selezionare un videogioco tramite ID
-  const sql = "SELECT * FROM videogames WHERE id = ? ;";
+  const sql = `
+  SELECT
+    v.id,
+    v.name,
+    v.description,
+    v.price,
+    v.promo_price,
+    v.developer,
+    v.release_date,
+    v.image_url,
+    v.quantity,
+    v.vote,
+    GROUP_CONCAT(DISTINCT p.name) AS platforms,
+    GROUP_CONCAT(DISTINCT g.name) AS genres,
+    GROUP_CONCAT(DISTINCT m.media_url) AS media_urls
+  FROM videogames AS v
+  LEFT JOIN platform_videogame AS pv ON v.id = pv.videogame_id
+  LEFT JOIN platforms AS p ON pv.platform_id = p.id
+  LEFT JOIN videogame_genre AS vg ON v.id = vg.videogame_id
+  LEFT JOIN genres AS g ON vg.genre_id = g.id
+  LEFT JOIN media AS m ON v.id = m.videogame_id
+  WHERE v.id = ?
+  GROUP BY v.id;`;
   // esegue la query sul database, passando l'ID come parametro
   connection.query(sql, [id], (err, results) => {
     // se si verifica un errore durante la connessione o l'esecuzione della query
     if (err) return res.status(500).json({ error: "Database query failed" });
     // se non viene trovato alcun risultato (l'ID non esiste nella tabella "posts"),
     if (results.length === 0)
-      return res.status(404).json({ error: "Videogioco not found" });
+      return res.status(404).json({ error: "Videogames not found" });
     // se l'elemento è stato trovato, lo restituisce come risposta JSON
-    return res.json(results[0]);
+
+    const formattedResult = results.map((result) => {
+      return {
+        id: result.id,
+        name: result.name,
+        description: result.description,
+        price: result.price,
+        promo_price: result.promo_price,
+        developer: result.developer,
+        release_date: result.release_date,
+        image_url: result.image_url,
+        quantity: result.quantity,
+        vote: result.vote,
+        platforms: result.platforms.split(","),
+        genres: result.genres.split(","),
+        media: result.media_urls.split(",").map((media_url) => {
+          if (media_url.includes("https://www.youtube.com/embed/")) {
+            return {
+              type: "video",
+              url: media_url,
+            };
+          } else {
+            return {
+              type: "img",
+              url: media_url,
+            };
+          }
+        }),
+      };
+    });
+
+    return res.json(formattedResult);
   });
-}
-
-/* store (create) */
-function store(req, res) {
-  // estraiamo i dati (name, description, price, ecc) dal corpo della richiesta HTTP
-  const {
-    name,
-    description,
-    price,
-    promo_price,
-    developer,
-    release_date,
-    image_url,
-    quantity,
-    vote,
-  } = req.body;
-  // definiamo la query SQL per inserire un nuovo videogioco
-  const sql = `
-    INSERT INTO videogames 
-    (name, description, price, promo_price, developer, release_date, image_url, quantity, vote)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `;
-  // esegue la query passando i valori ricevuti come parametri
-  connection.query(
-    sql,
-    [
-      name,
-      description,
-      price,
-      promo_price,
-      developer,
-      release_date,
-      image_url,
-      quantity,
-      vote,
-    ],
-    (err, results) => {
-      // se c'è un errore durante l'inserimento nel database
-      if (err)
-        return res.status(500).json({ error: "Failed to insert videogame" });
-      // se l'inserimento ha successo, restituisce lo status 201 (Created)
-      res.status(201);
-      // e un oggetto JSON contenente l'ID del nuovo post creato
-      res.json({ id: results.insertId });
-    }
-  );
-}
-
-/* update (edit) */
-function update(req, res) {
-  // estraiamo l'ID del videogioco dalla URL
-  const { id } = req.params;
-  // estraiamo i nuovi valori dal corpo della richiesta
-  const {
-    name,
-    description,
-    price,
-    promo_price,
-    developer,
-    release_date,
-    image_url,
-    quantity,
-    vote,
-  } = req.body;
-  // definiamo la query SQL per aggiornare un videogioco esistente
-  const sql = `
-    UPDATE videogames 
-    SET name = ?, description = ?, price = ?, promo_price = ?, developer = ?, release_date = ?, image_url = ?, quantity = ?, vote = ? 
-    WHERE id = ?
-  `;
-  // esegue la query, passando i nuovi valori e l'ID
-  connection.query(
-    sql,
-    [
-      name,
-      description,
-      price,
-      promo_price,
-      developer,
-      release_date,
-      image_url,
-      quantity,
-      vote,
-      id,
-    ],
-    (err, results) => {
-      // se si verifica un errore durante la query
-      if (err)
-        return res.status(500).json({ error: "Failed to update videogame" });
-      // se nessuna riga è stata modificata, il post con quell'ID non esiste
-      if (results.affectedRows === 0) {
-        // quindi restituisce un errore
-        return res.status(404).json({ error: "Videogame not found" });
-      }
-      // altrimenti conferma l'aggiornamento
-      res.json({ message: "Videogame update successfully" });
-    }
-  );
 }
 
 /* modify (partial edit) */
 function modify(req, res) {
   // estraiamo l'ID del videogioco dalla URL
   const { id } = req.params;
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid ID" });
   // estraiamo i nuovi valori dal corpo della richiesta
   const {
     name,
@@ -253,9 +208,9 @@ function modify(req, res) {
     values.push(vote);
   }
   // Se nessun campo è stato fornito per l'aggiornamento, restituisce errore
-  if (fields.length === 0) {
+  if (fields.length === 0)
     return res.status(400).json({ error: "No fields to update" });
-  }
+
   // Costruisce dinamicamente la query SQL usando solo i campi forniti
   const sql = `UPDATE videogames SET ${fields.join(", ")} WHERE id = ?`;
   // Aggiunge l'ID alla fine dell'array dei valori (serve per il WHERE)
@@ -274,26 +229,5 @@ function modify(req, res) {
   });
 }
 
-/* destroy (delete) */
-function destroy(req, res) {
-  // estrae l'ID dalla URL e lo converte da stringa a numero
-  const id = parseInt(req.params.id);
-  // definiamo la query SQL per eliminare un videogioco tramite ID
-  const sql = "DELETE FROM videogames WHERE id = ? ;";
-  // esegue la query sul database, passando l'ID come parametro
-  connection.query(sql, [id], (err, results) => {
-    // gestisce eventuali errori durante l'esecuzione della query
-    if (err)
-      return res.status(500).json({ error: "Failed to delete videogame" });
-    // verifichiamo se è stato effettivamente eliminato un elemento dalla tabella
-    if (results.affectedRows === 0) {
-      // se nessuna riga è stata eliminata, l'ID non esiste nel database e ci restituisce questo errore
-      return res.status(404).json({ error: "Videogame not found" });
-    }
-    // se l'eliminazione è avvenuta con successo, restituisce una conferma
-    return res.json({ message: "Videogioco deleted successfully" });
-  });
-}
-
 // esportiamo tutto
-export default { index, show, store, update, modify, destroy };
+export default { index, show, modify };
