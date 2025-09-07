32a947b42394f500f61cff0e5fb84d1cc8da0f5e
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-09-07 16:42:51 +0200

    btn gradient

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -13,151 +13,148 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <>
-      <div
-        className="row row-cols-1 row-cols-md-4 justify-content-center"
-        style={{ margin: 0, paddingTop: "2rem" }}
-      >
-        <div className="col-md-8 ">
+      <div className="container-fluid bg-dark text-white pt-4">
+        <div className="container">
           <hr />
 
-          <div className="row d-flex justify-content-between my-5">
-            <div className="col-4 d-flex justify-content-center">
-              <ul>
-                <li>
-                  <h3>About</h3>
-                </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+          {/* Sezione principale con 3 colonne responsive */}
+          <div className="row my-4">
+            {/* Colonna About */}
+            <div className="col-12 col-md-4 mb-4">
+              <h3 className="mb-3">About</h3>
+              <ul className="list-unstyled">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Informativa sulla privacy
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Cookie policy
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Contatti
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Chi siamo
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Note legali
                   </Link>
                 </li>
               </ul>
             </div>
-            <div className="col-4 d-flex justify-content-center">
-              <ul>
-                <li>
-                  <h3>Informazioni</h3>
-                </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+
+            {/* Colonna Informazioni */}
+            <div className="col-12 col-md-4 mb-4">
+              <h3 className="mb-3">Informazioni</h3>
+              <ul className="list-unstyled">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Politica di reso
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Assistenza clienti
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     FAQ
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Termini e condizioni
                   </Link>
                 </li>
-                <li>
-                  <Link to={"#"} className="text-decoration-none text-white">
+                <li className="mb-2">
+                  <Link to="#" className="text-decoration-none text-white">
                     Spedizioni
                   </Link>
                 </li>
               </ul>
             </div>
-            <div className="col-4 justify-content-center">
-              <h3 className="mb-5 text-center">Follow Us</h3>
 
-              <div className="d-flex justify-content-between flex-column">
-                <Link to={"#"} className="text-decoration-none text-white">
-                  <div className="d-flex align-items-center mb-2 gap-3">
+            {/* Colonna Follow Us */}
+            <div className="col-12 col-md-4 mb-4">
+              <h3 className="mb-3">Follow Us</h3>
+              <div className="d-flex flex-column gap-2">
+                <Link to="#" className="text-decoration-none text-white">
+                  <div className="d-flex align-items-center gap-3">
                     <div className="social-button ig">
                       <FontAwesomeIcon
                         icon={faInstagram}
                         className="social-icon"
                       />
                     </div>
-                    <p className="m-0">Instagram</p>
+                    <span>Instagram</span>
                   </div>
                 </Link>
 
-                <Link to={"#"} className="text-decoration-none text-white">
-                  <div className="d-flex align-items-center mb-2 gap-3">
+                <Link to="#" className="text-decoration-none text-white">
+                  <div className="d-flex align-items-center gap-3">
                     <div className="social-button fb">
                       <FontAwesomeIcon
                         icon={faFacebook}
                         className="social-icon"
                       />
                     </div>
-                    <p className="m-0">Facebook</p>
+                    <span>Facebook</span>
                   </div>
                 </Link>
 
-                <Link to={"#"} className="text-decoration-none text-white">
-                  <div className="d-flex align-items-center mb-2 gap-3">
+                <Link to="#" className="text-decoration-none text-white">
+                  <div className="d-flex align-items-center gap-3">
                     <div className="social-button discord">
                       <FontAwesomeIcon
                         icon={faDiscord}
                         className="social-icon"
                       />
                     </div>
-                    <p className="m-0">Discord</p>
+                    <span>Discord</span>
                   </div>
                 </Link>
 
-                <Link to={"#"} className="text-decoration-none text-white">
-                  <div className="d-flex align-items-center mb-2 gap-3">
+                <Link to="#" className="text-decoration-none text-white">
+                  <div className="d-flex align-items-center gap-3">
                     <div className="social-button x">
                       <FontAwesomeIcon
                         icon={faXTwitter}
                         className="social-icon"
                       />
                     </div>
-                    <p className="m-0">X / Twitter</p>
+                    <span>X / Twitter</span>
                   </div>
                 </Link>
 
-                <Link to={"#"} className="text-decoration-none text-white">
-                  <div className="d-flex align-items-center mb-2 gap-3">
+                <Link to="#" className="text-decoration-none text-white">
+                  <div className="d-flex align-items-center gap-3">
                     <div className="social-button youtube">
                       <FontAwesomeIcon
                         icon={faYoutube}
                         className="social-icon"
                       />
                     </div>
-                    <p className="m-0">YouTube</p>
+                    <span>YouTube</span>
                   </div>
                 </Link>
-
-                <Link to={"#"} className="text-decoration-none text-white">
-                  <div className="d-flex align-items-center mb-2 gap-3">
+                <Link to="#" className="text-decoration-none text-white">
+                  <div className="d-flex align-items-center gap-3">
                     <div className="social-button git">
                       <FontAwesomeIcon
                         icon={faGithub}
                         className="social-icon"
                       />
                     </div>
-                    <p className="m-0">GitHub</p>
+                    <span>GitHub</span>
                   </div>
                 </Link>
               </div>
@@ -193,12 +190,15 @@ export default function Footer() {
             </div>
           </div>
           <hr />
-          <p>© {new Date().getFullYear()} CloudGames.</p>
-          <p>
-            Tutti i diritti riservati. Tutti i marchi appartengono ai rispettivi
-            proprietari negli Stati Uniti e negli altri paesi. IVA inclusa nel
-            prezzo (ove applicabile)
-          </p>
+          {/* Copyright responsive */}
+          <div className="pb-4 text-center text-md-start">
+            <p className="mb-2">© {new Date().getFullYear()} CloudGames.</p>
+            <p className="mb-0  text-white">
+              Tutti i diritti riservati. Tutti i marchi appartengono ai
+              rispettivi proprietari negli Stati Uniti e negli altri paesi. IVA
+              inclusa nel prezzo (ove applicabile)
+            </p>
+          </div>
         </div>
       </div>
     </>
